94ea759a9ce05d92a2aeca7f55d70d366800a66c
Alex Mas <icalexm@gmail.com>
2024-05-08 15:05:34 +0200

    añadir evento tochstart

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+document.addEventListener("touchstart", (e) => {
+  pulsa(e.target);
+});
 document.addEventListener("click", (e) => {
-  console.log("e:", e, e.target);
-  const $el = e.target;
-  if ($el.classList.contains("posicion")) {
+  console.log("click:", e, e.target);
+  pulsa(e.target);
+});
+
+function pulsa(el) {
+  if (el.classList.contains("posicion")) {
     document.querySelectorAll(".aqui").forEach((element) => {
       element.classList.remove("aqui");
     });
-    $el.classList.add("aqui");
+    el.classList.add("aqui");
   }
-});
+}
