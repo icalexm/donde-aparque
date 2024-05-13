64668fc41e15e5a6853888e8efb75af275cc42a1
Alex Mas <icalexm@gmail.com>
2024-05-13 17:47:06 +0200

    Poder marcar assera i crear carres per js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const $miBarrio = window.miBarrio;
+
 document.addEventListener("touchstart", (e) => {
   pulsa(e.target);
 });
@@ -6,6 +8,7 @@ document.addEventListener("click", (e) => {
   pulsa(e.target);
 });
 document.addEventListener("DOMContentLoaded", (e) => {
+  pintaPantalla();
   const aqui = localStorage.getItem("marcat"),
     fechaStr = localStorage.getItem("marcatData");
   if (aqui) {
@@ -29,4 +32,76 @@ function pulsa(el) {
     //$input.value = localStorage.getItem("wpSearch");
     //localStorage.removeItem("wpSearch");
   }
+}
+
+function pintaPantalla() {
+  let str = "",
+    i = 1,
+    col = 3;
+  col = 0;
+  str += pintaCalle("calle-h", col, "", i);
+  col = 3;
+  i += col;
+  str += pintaCalle("calle-v", col, "", i);
+  i += col;
+  str += pintaCalle("calle-h", col, "", i);
+  i += col;
+
+  str += pintaCalle("calle-h", col, "Dia 13 (8 a 12)", i);
+  i += col;
+  str += pintaCalle("calle-v", col, "", i);
+  i += col;
+  str += pintaCalle("calle-h", col, "", i);
+  i += col;
+
+  str += pintaCalle("calle-h", col, "", i);
+  i += col;
+  str += pintaCalle("calle-v", col, "", i);
+  i += col;
+  str += pintaCalle("calle-h", col, "", i);
+  i += col;
+
+  str += pintaCalle("calle-h", col, "", i);
+  i += col;
+  col = 1;
+  str += pintaCalle("calle-v", col, "", i);
+  i += col;
+  $miBarrio.innerHTML = str;
+}
+
+function pintaCalle(calle, col, desc, id) {
+  if (col === 0) {
+    return `
+      <article class="${calle} ">
+      </article>
+    `;
+  }
+  let str = `
+      <article class="${calle} ">
+        <div class="desc">${desc}</div>
+        <div class="callePos">
+        `;
+  if (col >= 1) {
+    str += `
+        <div class="posicion" id="${id}a"></div>
+        <div class="posicion" id="${id}b"></div>
+      `;
+  }
+  if (col >= 2) {
+    str += `
+        <div class="posicion" id="${id + 1}b"></div>
+        <div class="posicion" id="${id + 1}a"></div>
+        `;
+  }
+  if (col > 2) {
+    str += `
+        <div class="posicion" id="${id + 2}a"></div>
+        <div class="posicion" id="${id + 2}b"></div>        
+        `;
+  }
+  str += `
+        </div>
+      </article>
+    `;
+  return str;
 }
